0ec557ab819bc92c6b120b359f0b5e65803215a6
Kanha Agrawal <kanha.agr11@gmail.com>
2021-07-07 20:49:37 +0530

    added post feature in second server

--- server2.js
+++ server2.js
@@ -1,8 +1,7 @@
 const http = require('http')
 const fetch = require('node-fetch')
-const axios = require('axios')
+const { getBodyData } = require('./utils/utils')
 const PORT = process.env.PORT || 2000
-const { getData, getDataById, addNewData, updateData, deleteData } = require('./controller/controller')
 const server = http.createServer((req, res) => {
     if (req.url === '/data' && req.method === 'GET') {
         fetch('http://localhost:1312/data').then(res => res.json()).then(json =>
@@ -11,18 +10,31 @@ const server = http.createServer((req, res) => {
     }
     else if (req.url.match(/\/data\/([0-9]+)/) && req.method === 'GET') {
         const id = req.url.split('/')[2]
-        getDataById(req, res, id)
+        // getDataById(req, res, id)
     }
     else if (req.url === '/data' && req.method === 'POST') {
-        addNewData(req, res)
+        getBodyData(req).then(body => {
+            const data = {
+                name: body.name,
+                price: body.price
+            }
+            fetch('http://localhost:1312/data', {
+                method: 'POST',
+                body: JSON.stringify(data),
+                headers: { 'Content-Type': 'application/json' }
+            }).then(res => res.json()).then(json =>
+                res.writeHead(201, { "Content-Type": "application/json" })
+                    .end(JSON.stringify(json)))
+        })
+        // addNewData(req, res)
     }
     else if (req.url.match(/\/data\/([0-9]+)/) && req.method === 'PUT') {
         const id = req.url.split('/')[2]
-        updateData(req, res, id)
+        // updateData(req, res, id)
     }
     else if (req.url.match(/\/data\/([0-9]+)/) && req.method === 'DELETE') {
         const id = req.url.split('/')[2]
-        deleteData(req, res, id)
+        // deleteData(req, res, id)
     }
     else {
         res.writeHead(404, { "Content-Type": "application/json" })
